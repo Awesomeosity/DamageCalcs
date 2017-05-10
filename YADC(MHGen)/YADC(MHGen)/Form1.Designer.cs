47e052c4e0acf7db60b6651267630381ab5ecb44
Kevin Le <infernapeman@gmail.com>
2017-05-10 16:26:50 -0600

    Hunter Parameter section works.

--- YADC(MHGen)/YADC(MHGen)/Form1.Designer.cs
+++ YADC(MHGen)/YADC(MHGen)/Form1.Designer.cs
@@ -50,7 +50,6 @@
             this.AffinityField = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
             this.SharpnessLabel = new System.Windows.Forms.Label();
-            this.SharpField = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.EleField = new System.Windows.Forms.TextBox();
             this.HiddenField = new System.Windows.Forms.TextBox();
@@ -60,13 +59,24 @@
             this.label12 = new System.Windows.Forms.Label();
             this.EleZoneField = new System.Windows.Forms.TextBox();
             this.label13 = new System.Windows.Forms.Label();
-            this.OutLabel = new System.Windows.Forms.Label();
+            this.RawOut = new System.Windows.Forms.Label();
             this.QuestField = new System.Windows.Forms.TextBox();
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.FinalVal = new System.Windows.Forms.Label();
             this.CalcButt = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.EleOut = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.sharpnessBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.ErrorPreventer)).BeginInit();
             this.SuspendLayout();
             // 
@@ -91,9 +101,9 @@
             this.RawLabel.AutoSize = true;
             this.RawLabel.Location = new System.Drawing.Point(9, 298);
             this.RawLabel.Name = "RawLabel";
-            this.RawLabel.Size = new System.Drawing.Size(99, 13);
+            this.RawLabel.Size = new System.Drawing.Size(101, 13);
             this.RawLabel.TabIndex = 1;
-            this.RawLabel.Text = "Raw Attack Power:";
+            this.RawLabel.Text = "Total Attack Power:";
             // 
             // MVField
             // 
@@ -118,11 +128,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(334, 342);
+            this.label2.Location = new System.Drawing.Point(213, 404);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "Damage Output:";
+            this.label2.Text = "Raw Damage:";
             // 
             // TypeLabel
             // 
@@ -243,7 +253,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(318, 317);
+            this.label8.Location = new System.Drawing.Point(315, 318);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(20, 16);
             this.label8.TabIndex = 19;
@@ -254,20 +264,9 @@
             this.SharpnessLabel.AutoSize = true;
             this.SharpnessLabel.Location = new System.Drawing.Point(111, 342);
             this.SharpnessLabel.Name = "SharpnessLabel";
-            this.SharpnessLabel.Size = new System.Drawing.Size(84, 13);
+            this.SharpnessLabel.Size = new System.Drawing.Size(60, 13);
             this.SharpnessLabel.TabIndex = 20;
-            this.SharpnessLabel.Text = "Sharpness Mod:";
-            // 
-            // SharpField
-            // 
-            this.SharpField.Location = new System.Drawing.Point(114, 358);
-            this.SharpField.Name = "SharpField";
-            this.SharpField.Size = new System.Drawing.Size(96, 20);
-            this.SharpField.TabIndex = 21;
-            this.SharpField.Text = "1.0";
-            this.SharpField.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.SharpField.Validating += new System.ComponentModel.CancelEventHandler(this.GenericField_Validating);
-            this.SharpField.Validated += new System.EventHandler(this.GenericField_Validated);
+            this.SharpnessLabel.Text = "Sharpness:";
             // 
             // label9
             // 
@@ -312,14 +311,14 @@
             // label11
             // 
             this.label11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label11.Location = new System.Drawing.Point(453, 9);
+            this.label11.Location = new System.Drawing.Point(341, 9);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(2, 372);
+            this.label11.Size = new System.Drawing.Size(2, 484);
             this.label11.TabIndex = 26;
             // 
             // HitzoneField
             // 
-            this.HitzoneField.Location = new System.Drawing.Point(471, 318);
+            this.HitzoneField.Location = new System.Drawing.Point(352, 314);
             this.HitzoneField.Name = "HitzoneField";
             this.HitzoneField.Size = new System.Drawing.Size(96, 20);
             this.HitzoneField.TabIndex = 27;
@@ -331,7 +330,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(468, 302);
+            this.label12.Location = new System.Drawing.Point(349, 298);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(76, 13);
             this.label12.TabIndex = 28;
@@ -339,7 +338,7 @@
             // 
             // EleZoneField
             // 
-            this.EleZoneField.Location = new System.Drawing.Point(471, 358);
+            this.EleZoneField.Location = new System.Drawing.Point(352, 354);
             this.EleZoneField.Name = "EleZoneField";
             this.EleZoneField.Size = new System.Drawing.Size(96, 20);
             this.EleZoneField.TabIndex = 29;
@@ -351,26 +350,26 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(471, 341);
+            this.label13.Location = new System.Drawing.Point(349, 338);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(97, 13);
             this.label13.TabIndex = 30;
             this.label13.Text = "Ele. Hitzone Value:";
             // 
-            // OutLabel
+            // RawOut
             // 
-            this.OutLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.OutLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.OutLabel.Location = new System.Drawing.Point(337, 357);
-            this.OutLabel.Name = "OutLabel";
-            this.OutLabel.Size = new System.Drawing.Size(96, 20);
-            this.OutLabel.TabIndex = 31;
-            this.OutLabel.Text = "0";
-            this.OutLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.RawOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.RawOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.RawOut.Location = new System.Drawing.Point(216, 417);
+            this.RawOut.Name = "RawOut";
+            this.RawOut.Size = new System.Drawing.Size(96, 20);
+            this.RawOut.TabIndex = 31;
+            this.RawOut.Text = "0";
+            this.RawOut.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // QuestField
             // 
-            this.QuestField.Location = new System.Drawing.Point(573, 358);
+            this.QuestField.Location = new System.Drawing.Point(454, 354);
             this.QuestField.Name = "QuestField";
             this.QuestField.Size = new System.Drawing.Size(96, 20);
             this.QuestField.TabIndex = 32;
@@ -382,7 +381,7 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(570, 342);
+            this.label14.Location = new System.Drawing.Point(451, 338);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(62, 13);
             this.label14.TabIndex = 33;
@@ -391,7 +390,7 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(688, 341);
+            this.label15.Location = new System.Drawing.Point(569, 337);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(75, 13);
             this.label15.TabIndex = 34;
@@ -401,7 +400,7 @@
             // 
             this.FinalVal.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.FinalVal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.FinalVal.Location = new System.Drawing.Point(691, 357);
+            this.FinalVal.Location = new System.Drawing.Point(572, 353);
             this.FinalVal.Name = "FinalVal";
             this.FinalVal.Size = new System.Drawing.Size(96, 20);
             this.FinalVal.TabIndex = 35;
@@ -410,34 +409,161 @@
             // 
             // CalcButt
             // 
-            this.CalcButt.Location = new System.Drawing.Point(337, 314);
+            this.CalcButt.Location = new System.Drawing.Point(114, 455);
             this.CalcButt.Name = "CalcButt";
             this.CalcButt.Size = new System.Drawing.Size(96, 23);
             this.CalcButt.TabIndex = 36;
             this.CalcButt.Text = "Calculate Output";
             this.CalcButt.UseVisualStyleBackColor = true;
+            this.CalcButt.Click += new System.EventHandler(this.CalcButt_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(691, 318);
+            this.button1.Location = new System.Drawing.Point(572, 314);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 23);
             this.button1.TabIndex = 37;
             this.button1.Text = "Calculate All";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // label16
+            // 
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label16.Location = new System.Drawing.Point(12, 292);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(656, 2);
+            this.label16.TabIndex = 38;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(12, 276);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(95, 13);
+            this.label17.TabIndex = 39;
+            this.label17.Text = "Hunter Parameters";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(349, 272);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(101, 13);
+            this.label18.TabIndex = 40;
+            this.label18.Text = "Monster Parameters";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(213, 443);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(99, 13);
+            this.label19.TabIndex = 41;
+            this.label19.Text = "Elemental Damage:";
+            // 
+            // EleOut
+            // 
+            this.EleOut.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.EleOut.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.EleOut.Location = new System.Drawing.Point(216, 456);
+            this.EleOut.Name = "EleOut";
+            this.EleOut.Size = new System.Drawing.Size(96, 20);
+            this.EleOut.TabIndex = 42;
+            this.EleOut.Text = "0";
+            this.EleOut.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(9, 387);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(89, 13);
+            this.label20.TabIndex = 43;
+            this.label20.Text = "Calculation Type:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 405);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 17);
+            this.radioButton1.TabIndex = 48;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Average";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 422);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(96, 17);
+            this.radioButton2.TabIndex = 49;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Positive Affinity";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 439);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(102, 17);
+            this.radioButton3.TabIndex = 50;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Negative Affinity";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 456);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 17);
+            this.radioButton4.TabIndex = 51;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Neutral";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // sharpnessBox
+            // 
+            this.sharpnessBox.FormattingEnabled = true;
+            this.sharpnessBox.Items.AddRange(new object[] {
+            "(No Sharpness)",
+            "White",
+            "Blue",
+            "Green",
+            "Yellow",
+            "Orange",
+            "Red"});
+            this.sharpnessBox.Location = new System.Drawing.Point(114, 356);
+            this.sharpnessBox.Name = "sharpnessBox";
+            this.sharpnessBox.Size = new System.Drawing.Size(96, 21);
+            this.sharpnessBox.TabIndex = 52;
+            // 
             // DmgCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(813, 390);
+            this.ClientSize = new System.Drawing.Size(675, 502);
+            this.Controls.Add(this.sharpnessBox);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.EleOut);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.CalcButt);
             this.Controls.Add(this.FinalVal);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.QuestField);
-            this.Controls.Add(this.OutLabel);
+            this.Controls.Add(this.RawOut);
             this.Controls.Add(this.label13);
             this.Controls.Add(this.EleZoneField);
             this.Controls.Add(this.label12);
@@ -447,7 +573,6 @@
             this.Controls.Add(this.HiddenField);
             this.Controls.Add(this.EleField);
             this.Controls.Add(this.label9);
-            this.Controls.Add(this.SharpField);
             this.Controls.Add(this.SharpnessLabel);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.AffinityField);
@@ -498,7 +623,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.TextBox AffinityField;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox SharpField;
         private System.Windows.Forms.Label SharpnessLabel;
         private System.Windows.Forms.TextBox EleField;
         private System.Windows.Forms.Label label9;
@@ -507,7 +631,7 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox HitzoneField;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label OutLabel;
+        private System.Windows.Forms.Label RawOut;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.TextBox EleZoneField;
         private System.Windows.Forms.TextBox QuestField;
@@ -516,6 +640,17 @@
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button CalcButt;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label EleOut;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ComboBox sharpnessBox;
     }
 }
 
--- YADC(MHGen)/YADC(MHGen)/Form1.cs
+++ YADC(MHGen)/YADC(MHGen)/Form1.cs
@@ -12,9 +12,15 @@ namespace YADC_MHGen_
 {
     public partial class DmgCalculator : Form
     {
+
+        public Dictionary<string, Tuple<double, double>> sharpnessValues = new Dictionary<string, Tuple<double, double>>();
+
         public DmgCalculator()
         {
             InitializeComponent();
+            this.sharpnessBox.SelectedIndex = 0;
+            radioButton1.Select();
+            FillOut();
         }
 
         /*Generic Field Validation*/
@@ -36,19 +42,38 @@ namespace YADC_MHGen_
         }
         
 
+        /*CalcButt Functions*/
+        private void CalcButt_Click(object sender, System.EventArgs e) //TO BE MODIFIED
+        { //REASON: NEED TO CALCULATE AFFINITY HITS (NEGATIVE, POSITIVE, NEUTRAL), AS WELL AS ON AVERAGE
+            double total     = double.Parse(RawField.Text);
+            double motion    = double.Parse(MVField.Text) * 0.01;
+            double affinity  = double.Parse(AffinityField.Text) * 0.01;
+            double element   = double.Parse(EleField.Text);
+            double hidden    = double.Parse(HiddenField.Text);
+
+            double rawSharp = sharpnessValues[sharpnessBox.Text].Item1;
+            double eleSharp = sharpnessValues[sharpnessBox.Text].Item2;
+
+            double rawTotal = total * (1 + affinity * 0.25) * rawSharp * hidden * motion;
+            double eleTotal = element * eleSharp * hidden;
+
+            RawOut.Text = rawTotal.ToString();
+            EleOut.Text = eleTotal.ToString();
+        }
+
 
         /*Functions*/
         private void damageCalc(string RawDamage, string MotionValue) //TO BE MODIFIED
         {
             float raw = float.Parse(RawDamage);
             float MV = float.Parse(MotionValue);
-            OutLabel.Text = (raw * MV).ToString();
+            RawOut.Text = (raw * MV).ToString();
         }
 
         public bool calcFieldValidation(string input, out string ErrorMessage)
         {
-            float result;
-            if(!float.TryParse(input, out result))
+            double result;
+            if(!double.TryParse(input, out result))
             {
                 ErrorMessage = "Enter in a valid number.";
                 return false;
@@ -59,5 +84,17 @@ namespace YADC_MHGen_
                 return true;
             }
         }
+
+        private void FillOut()
+        {
+            sharpnessValues.Add("(No Sharpness)",   new Tuple<double, double>(1.00, 1.00));
+            sharpnessValues.Add("White",            new Tuple<double, double>(1.32, 1.12));
+            sharpnessValues.Add("Blue",             new Tuple<double, double>(1.20, 1.06));
+            sharpnessValues.Add("Green",            new Tuple<double, double>(1.05, 1.0));
+            sharpnessValues.Add("Yellow",           new Tuple<double, double>(1.00, 0.75));
+            sharpnessValues.Add("Orange",           new Tuple<double, double>(0.75, 0.50));
+            sharpnessValues.Add("Red",              new Tuple<double, double>(0.50, 0.25));
+        }
+        
     }
 }
